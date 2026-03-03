dc7919e5fa5596dd0efe6db1c39e0094052989fb
Anukul087 <proanukul087@gmail.com>
2026-03-03 07:31:20 +0530

    Updated branding with Mini CRM logo

--- mini-crm/server/server.js
+++ mini-crm/server/server.js
@@ -25,6 +25,8 @@ async function connectDB() {
 
 connectDB();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
